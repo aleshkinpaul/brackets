1f4cbd5d2a20403825a8ef72b6460e4ed9377ff2
Aleshkin Paul <vinci.aleshkin@gmail.com>
2020-03-19 20:56:10 +0300

    feat: add index.js for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const masStr = str.split(''), masTmp = [];
+  let masOpenBracket = [], masCloseBracket = [], masSameBracket = [], masSameBracketCounter = [];
+
+  bracketsConfig.forEach((item, i) => {
+    if (item[0] === item[1]) {
+      masSameBracket.push(item[0]);
+      masSameBracketCounter.push(0);
+    }
+    else {
+      masOpenBracket.push(item[0]);
+      masCloseBracket.push(item[1]);
+    }
+  });
+
+  for (let i = 0; i < masStr.length; i++) {
+    const item = masStr[i];
+
+    if (masSameBracket.includes(item)) {
+      let indexBracketInConfig = masSameBracket.indexOf(item);
+
+      if (masSameBracketCounter[indexBracketInConfig] === 0) {
+        masTmp.push(item);
+        masSameBracketCounter[indexBracketInConfig] = 1;
+      }
+      else {
+        if (masTmp[masTmp.length - 1] === item) {
+          masTmp.pop();
+          masSameBracketCounter[indexBracketInConfig] = 0;
+        }
+        else {
+          return false;
+        }
+      }
+    }
+    else {
+      if (masCloseBracket.includes(item)) {
+        let indexBracketInConfig = masCloseBracket.indexOf(item);
+
+        if (masTmp.length === 0 || masTmp[masTmp.length - 1] !== masOpenBracket[indexBracketInConfig]) return false;
+
+        masTmp.pop();
+      }
+
+      if (masOpenBracket.includes(item)) masTmp.push(item);
+    }
+  };
+
+  return masTmp.length === 0 ? true : false;
 }
